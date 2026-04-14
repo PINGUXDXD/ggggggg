3c37daeed8cc4110f804d920750e22eaf96a57d1
PINGUXDXD <matiasn232006@gmail.com>
2026-04-14 13:30:44 -0400

    mas ejemplos de for

--- GG.py
+++ GG.py
@@ -4,3 +4,25 @@
 # num=int(input("ingrese un numero: "))
 # for i in range(10):
 #  print(num, "x", i+1 , "=", num*(i+1))
+
+#pedir al usuario la cantidad de notas 
+#sacar el promedio para esa cantidad de notas 
+# num=int(input("ingrese la cantidad de notas: "))
+# sum=0
+
+
+# for i in range(num):
+#   nota=float(input("ingresa la nota; "))
+#   sum=sum+nota
+# prom=sum/num
+# print("el promedio es", prom) 
+# if prom>=4.0:
+#   print("el alumno aprobo")
+# else:
+#   print("el alumno reprobo")
+# cantvocal=0
+# nombre=input("ingrese su nombre ")
+# for i in nombre:
+#     print(i)
+#     cantletra=cantletra+1
+# print("la cantidad de letras que tiene su nombre es de ", cantletra)
