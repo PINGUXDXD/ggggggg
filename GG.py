b157efa886a77b78e66d7592b82d018d570fd13e
PINGUXDXD <matiasn232006@gmail.com>
2026-04-14 12:48:36 -0400

    ejemplo de for

---
+++ GG.py
@@ -0,0 +1,6 @@
+# num=int(input("ingrese un numero: "))
+# for i in range (num): 
+#     print(i+1,"hola usuario ")
+# num=int(input("ingrese un numero: "))
+# for i in range(10):
+#  print(num, "x", i+1 , "=", num*(i+1))
--- OMG.py
+++ OMG.py
@@ -1,5 +1,17 @@
-print("ingrese 2 numeros ")
-numero1=int(print("ingrese primer numero: "))
-numero2=int(print("ingrese seegundo numero: "))
-print("el resultadode la suma es ", numero1+numero2 )
-print("el resultado de la resta es ", numero1-numero2)
+print(
+    ''''
+    1.-pera 1200
+    2.- manzana 1500
+    3.- piña 2000
+
+ ''')
+select=int(print("seleccione una fruta: ")) 
+if select==1:
+      print("el total a pagar es de ", 1200*1.19)
+else:
+        if select==2:
+          print("el total a pagar es de ", 1500*1.19)
+        else: 
+            if select==3:
+               print("el total a pagar es de ", 2000*1.19)
+            else 
