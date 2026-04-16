ef405361e41b205319f36240e0da51fcd1c0b326
PINGUXDXD <matiasn232006@gmail.com>
2026-04-16 13:15:45 -0400

    nuevos ejemplos de python

---
+++ GGG.py
@@ -0,0 +1,18 @@
+# titulo="clima actual " 
+# diademes=16
+# mes=4
+# llueve=False
+# print(f" (titulo) ")
+# name=input("ingrese su nombre ")
+# cantletras=0
+# for i in name:
+#  print(i)
+#  cantletras=cantletras+1
+# print(f"la cant de letras son: {cantletras}")
+# nombre=input("ingrese su nombre: ")
+# vocales=0
+# for i in nombre:
+#   print (i)
+#   if i in "aeiou":
+#     vocales+=1
+# print(f"la cant de vocales {vocales}")
